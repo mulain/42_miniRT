310eaa65dbea10e91d84f8054edc5138758e4b43
wolf <wolf.mardin@gmail.com>
2023-02-16 10:54:56 +0100

    auto add & push

--- src/utils_print1.c
+++ src/utils_print1.c
@@ -20,46 +20,46 @@ void	print_objlist(t_objlist *lst)
 
 void	print_ambientlight(t_data *d)
 {
-	ft_printf("----- Ambient Light -----\n");
-	ft_printf("Brightness:\n");
+	printf("----- Ambient Light -----\n");
+	printf("Brightness:\n");
 	printf("  %f\n", d->amb_light.brightness);
-	ft_printf("Color:\n");
-	ft_printf("  r: %i\n", d->amb_light.color.r);
-	ft_printf("  g: %i\n", d->amb_light.color.g);
-	ft_printf("  b: %i\n", d->amb_light.color.b);
+	printf("Color:\n");
+	printf("  r: %i\n", d->amb_light.color.r);
+	printf("  g: %i\n", d->amb_light.color.g);
+	printf("  b: %i\n", d->amb_light.color.b);
 }
 
 void	print_camera(t_data *d)
 {
-	ft_printf("----- Camera -----\n");
-	ft_printf("Viewpoint:\n");
+	printf("----- Camera -----\n");
+	printf("Viewpoint:\n");
 	printf("  x: %f\n", d->camera.viewpoint.x);
 	printf("  y: %f\n", d->camera.viewpoint.y);
 	printf("  z: %f\n", d->camera.viewpoint.z);
-	ft_printf("Normvector:\n");
+	printf("Normvector:\n");
 	printf("  x: %f\n", d->camera.normvector.x);
 	printf("  y: %f\n", d->camera.normvector.y);
 	printf("  z: %f\n", d->camera.normvector.z);
-	ft_printf("Field of view:\n");
-	ft_printf("  %i degrees\n", d->camera.fieldofview);
+	printf("Field of view:\n");
+	printf("  %i degrees\n", d->camera.fieldofview);
 }
 
 void	print_light(t_data *d)
 {
 	if (!d->light.declared)
 	{
-		ft_printf("Light not declared.\n");
+		printf("Light not declared.\n");
 		return ;
 	}
-	ft_printf("----- Light -----\n");
-	ft_printf("Coordinates:\n");
+	printf("----- Light -----\n");
+	printf("Coordinates:\n");
 	printf("  x: %f\n", d->light.coordinates.x);
 	printf("  y: %f\n", d->light.coordinates.y);
 	printf("  z: %f\n", d->light.coordinates.z);
-	ft_printf("Brightness:\n");
+	printf("Brightness:\n");
 	printf("  %f\n", d->light.brightness);
-	ft_printf("Color:\n");
-	ft_printf("  r: %i\n", d->light.color.r);
-	ft_printf("  g: %i\n", d->light.color.g);
-	ft_printf("  b: %i\n", d->light.color.b);
+	printf("Color:\n");
+	printf("  r: %i\n", d->light.color.r);
+	printf("  g: %i\n", d->light.color.g);
+	printf("  b: %i\n", d->light.color.b);
 }
--- src/utils_print2.c
+++ src/utils_print2.c
@@ -3,53 +3,53 @@
 
 void	print_sphere(t_sphere *sp)
 {
-	ft_printf("----- Sphere -----\n");
-	ft_printf("Coordinates:\n");
+	printf("----- Sphere -----\n");
+	printf("Coordinates:\n");
 	printf("  x: %f\n", sp->center.x);
 	printf("  y: %f\n", sp->center.x);
 	printf("  z: %f\n", sp->center.x);
-	ft_printf("Diameter:\n");
+	printf("Diameter:\n");
 	printf("  %f\n", sp->diameter);
-	ft_printf("Color:\n");
-	ft_printf("  r: %i\n", sp->color.r);
-	ft_printf("  g: %i\n", sp->color.g);
-	ft_printf("  b: %i\n", sp->color.b);
+	printf("Color:\n");
+	printf("  r: %i\n", sp->color.r);
+	printf("  g: %i\n", sp->color.g);
+	printf("  b: %i\n", sp->color.b);
 }
 
 void	print_plane(t_plane *pl)
 {
-	ft_printf("----- Plane -----\n");
-	ft_printf("Anchor:\n");
+	printf("----- Plane -----\n");
+	printf("Anchor:\n");
 	printf("  x: %f\n", pl->anchor.x);
 	printf("  y: %f\n", pl->anchor.y);
 	printf("  z: %f\n", pl->anchor.z);
-	ft_printf("Normvector:\n");
+	printf("Normvector:\n");
 	printf("  x: %f\n", pl->normvector.x);
 	printf("  y: %f\n", pl->normvector.y);
 	printf("  z: %f\n", pl->normvector.z);
-	ft_printf("Color:\n");
-	ft_printf("  r: %i\n", pl->color.r);
-	ft_printf("  g: %i\n", pl->color.g);
-	ft_printf("  b: %i\n", pl->color.b);
+	printf("Color:\n");
+	printf("  r: %i\n", pl->color.r);
+	printf("  g: %i\n", pl->color.g);
+	printf("  b: %i\n", pl->color.b);
 }
 
 void	print_cylinder(t_cylinder *cy)
 {
-	ft_printf("----- Cylinder -----\n");
-	ft_printf("Anchor:\n");
+	printf("----- Cylinder -----\n");
+	printf("Anchor:\n");
 	printf("  x: %f\n", cy->anchor.x);
 	printf("  y: %f\n", cy->anchor.y);
 	printf("  z: %f\n", cy->anchor.z);
-	ft_printf("Normvector:\n");
+	printf("Normvector:\n");
 	printf("  x: %f\n", cy->normvector.x);
 	printf("  y: %f\n", cy->normvector.y);
 	printf("  z: %f\n", cy->normvector.z);
-	ft_printf("Diameter:\n");
+	printf("Diameter:\n");
 	printf("  %f\n", cy->diameter);
-	ft_printf("Height:\n");
+	printf("Height:\n");
 	printf("  %f\n", cy->height);
-	ft_printf("Color:\n");
-	ft_printf("  r: %i\n", cy->color.r);
-	ft_printf("  g: %i\n", cy->color.g);
-	ft_printf("  b: %i\n", cy->color.b);
+	printf("Color:\n");
+	printf("  r: %i\n", cy->color.r);
+	printf("  g: %i\n", cy->color.g);
+	printf("  b: %i\n", cy->color.b);
 }
