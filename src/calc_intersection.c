895f169c9a7e419e905245e68de6ce434e5ada1a
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-02-25 14:45:40 +0100

    auto add & push

--- src/calc_intersection.c
+++ src/calc_intersection.c
@@ -225,23 +225,73 @@ typedef struct {
 
 double intersectCylinder(Ray ray, Cylinder cylinder) {
     // Transform the ray and cylinder to a common coordinate system
+	t_vector	to_cyl;
+	t_vector	projction;
+	t_vector	to_closestpoint;
+	t_vector	to_top;
+	t_vector	to_bottom;
+	t_vector	intersection;
+
+	double		dotprod;
+	double		dist_axis;
+	double		dist_surface;
+	double		dist_intersct;
+	double		dist_top;
+	double		dist_bottom;
+
+	t_cylinder	cyl;
+	t_ray		ry;
+
+
     Vec3 toCylinder = {cylinder.center.x - ray.origin.x, cylinder.center.y - ray.origin.y, cylinder.center.z - ray.origin.z};
-    double dotProduct = ray.direction.x * cylinder.axis.x + ray.direction.y * cylinder.axis.y + ray.direction.z * cylinder.axis.z;
-    Vec3 projection = {dotProduct * cylinder.axis.x, dotProduct * cylinder.axis.y, dotProduct * cylinder.axis.z};
-    Vec3 toClosestPoint = {toCylinder.x - projection.x, toCylinder.y - projection.y, toCylinder.z - projection.z};
-    double distanceToAxis = sqrt(toClosestPoint.x * toClosestPoint.x + toClosestPoint.y * toClosestPoint.y + toClosestPoint.z * toClosestPoint.z);
-    double distanceToSurface = sqrt(cylinder.radius * cylinder.radius - distanceToAxis * distanceToAxis);
+	to_cyl = point_subtract(cyl.point, ry.point);
+    
+	double dotProduct = ray.direction.x * cylinder.axis.x + ray.direction.y * cylinder.axis.y + ray.direction.z * cylinder.axis.z;
+    dotprod = vector_dotprod(ry.vector, cyl.vector);
+
+	Vec3 projection = {dotProduct * cylinder.axis.x, dotProduct * cylinder.axis.y, dotProduct * cylinder.axis.z};
+    projction = vector_multiply(cyl.vector, dotprod);
+
+	Vec3 toClosestPoint = {toCylinder.x - projection.x, toCylinder.y - projection.y, toCylinder.z - projection.z};
+    to_closestpoint = point_subtract(to_cyl, projction);
+	
+	double distanceToAxis = sqrt(toClosestPoint.x * toClosestPoint.x + toClosestPoint.y * toClosestPoint.y + toClosestPoint.z * toClosestPoint.z);
+    dist_axis = vector_length(to_closestpoint);
+	
+	double distanceToSurface = sqrt(cylinder.radius * cylinder.radius - distanceToAxis * distanceToAxis);
+	dist_surface = sqrt(cyl.radius * cyl.radius - dist_axis * dist_axis);
+
     double distanceToIntersection = dotProduct - distanceToSurface;
+	dist_intersct = dotprod - dist_surface;
+
     Vec3 intersectionPoint = {ray.origin.x + distanceToIntersection * ray.direction.x, ray.origin.y + distanceToIntersection * ray.direction.y, ray.origin.z + distanceToIntersection * ray.direction.z};
-    Vec3 axisDir = {cylinder.axis.x, cylinder.axis.y, cylinder.axis.z};
+    intersection.x = ry.point.x + dist_intersct * ry.vector.x;
+	intersection.y = ry.point.y + dist_intersct * ry.vector.y;
+	intersection.z = ry.point.z + dist_intersct * ry.vector.z;
+	
+	Vec3 axisDir = {cylinder.axis.x, cylinder.axis.y, cylinder.axis.z};
+	//why?
+
     Vec3 toTop = {cylinder.center.x + axisDir.x * cylinder.height / 2.0 - intersectionPoint.x,
                   cylinder.center.y + axisDir.y * cylinder.height / 2.0 - intersectionPoint.y,
                   cylinder.center.z + axisDir.z * cylinder.height / 2.0 - intersectionPoint.z};
+	to_top.x = cyl.point.x + cyl.vector.x * cyl.height / 2 - intersection.x;
+	to_top.y = cyl.point.y + cyl.vector.y * cyl.height / 2 - intersection.y;
+	to_top.z = cyl.point.z + cyl.vector.z * cyl.height / 2 - intersection.z;
+
     Vec3 toBottom = {cylinder.center.x - axisDir.x * cylinder.height / 2.0 - intersectionPoint.x,
                      cylinder.center.y - axisDir.y * cylinder.height / 2.0 - intersectionPoint.y,
                      cylinder.center.z - axisDir.z * cylinder.height / 2.0 - intersectionPoint.z};
+	to_bottom.x = cyl.point.x - cyl.vector.x * cyl.height / 2 - intersection.x;
+	to_bottom.y = cyl.point.y - cyl.vector.y * cyl.height / 2 - intersection.y;
+	to_bottom.z = cyl.point.z - cyl.vector.z * cyl.height / 2 - intersection.z;
+
     double distanceToTop = sqrt(toTop.x * toTop.x + toTop.y * toTop.y + toTop.z * toTop.z);
-    double distanceToBottom = sqrt(toBottom.x * toBottom.x + toBottom.y * toBottom.y + toBottom.z * toBottom.z);
+	dist_top = vector_length(to_top);
+    
+	double distanceToBottom = sqrt(toBottom.x * toBottom.x + toBottom.y * toBottom.y + toBottom.z * toBottom.z);
+	dist_bottom = vector_length(to_bottom);
+
     double t = -1;
     if (distanceToIntersection < 0) {
         // The intersection point is behind the ray
