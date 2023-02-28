62816aca1b2bffb1a493f797e0b8c7ad9a8bf302
wolf <wolf.mardin@gmail.com>
2023-02-28 08:43:25 +0100

    auto add & push

--- src/intersections1.c
+++ src/intersections1.c
@@ -5,40 +5,37 @@ t_intrsct	intersect_plane(t_ray ray, t_plane plane)
 {
 	double		divisor;
 	double		result;
-	t_intrsct	intersection;
+	t_intrsct	i;
 
-	intersection.color = plane.color.trgb;
+	i.color = plane.color.trgb;
 	divisor = dot(plane.vector, ray.direction);
 	if (fabs(divisor) < EPSILON)
-	{
-		intersection.distance = INFINITY;
-		return (intersection);
-	}
+		return (i.distance = INFINITY, i);
 	result = dot(subtract(plane.point, ray.origin), plane.vector) / divisor;
 	if (result < EPSILON)
-		intersection.distance = INFINITY;
+		i.distance = INFINITY;
 	else
-		intersection.distance = result;
-	return (intersection);
+		i.distance = result;
+	return (i);
 }
 
 t_intrsct	intersect_sphere(t_ray ray, t_sphere sphere)
 {
 	t_3d		oc;
 	t_helper	h;
-	t_intrsct	intersection;
+	t_intrsct	i;
 
-	intersection.color = sphere.color.trgb;
+	i.color = sphere.color.trgb;
 	oc = subtract(ray.origin, sphere.center);
 	h.a = dot(ray.direction, ray.direction);
 	h.b = 2 * dot(ray.direction, oc);
 	h.c = dot(oc, oc) - sphere.radius * sphere.radius;
 	h.discriminant = h.b * h.b - 4 * h.a * h.c;
 	if (h.discriminant < EPSILON)
-		intersection.distance = INFINITY;
+		i.distance = INFINITY;
 	else
-		intersection.distance = (-h.b - sqrt(h.discriminant)) / (2 * h.a);
-	return (intersection);
+		i.distance = (-h.b - sqrt(h.discriminant)) / (2 * h.a);
+	return (i);
 }
 
 t_intrsct	intersect_cylinder(t_ray ray, t_cylinder cylinder)
@@ -70,15 +67,15 @@ double	cylinder_tube(t_ray ray, t_cylinder cylinder)
 	h.b = 2 * dot(v, u);
 	h.c = dot(u, u) - cylinder.radius * cylinder.radius;
 	h.discriminant = h.b * h.b - 4 * h.a * h.c;
-	if (h.discriminant < 0)
+	if (h.discriminant < EPSILON)
 		return (INFINITY);
 	h.t1 = (-h.b - sqrt(h.discriminant)) / (2 * h.a);
-	if (h.t1 < 0)
+	if (h.t1 < EPSILON)
 		return (INFINITY);
 	h.intrsct = add(ray.origin, mult(ray.direction, h.t1));
 	h.intrsct = subtract(h.intrsct, cylinder.base);
 	h.dot_prod = dot(h.intrsct, cylinder.axis);
-	if (h.dot_prod < 0 || h.dot_prod > cylinder.height)
+	if (h.dot_prod < EPSILON || h.dot_prod > cylinder.height)
 		return (INFINITY);
 	return (h.t1);
 }
