62b178350442592692c0b7e866c7f3e2f345db66
wolf <wolf.mardin@gmail.com>
2023-03-10 11:59:32 +0100

    auto add & push

--- src/parsing/parsing_lightsandcam.c
+++ src/parsing/parsing_lightsandcam.c
@@ -40,7 +40,7 @@ void	parse_camera(t_data *d)
 	d->parse.min = 0;
 	d->parse.max = (double)180;
 	d->camera.fieldofview = parse_int(d, d->parse.elmnts[3], E_CAMERA3);
-	d->fov_ratio = tan((d->camera.fieldofview / 2) * M_PI / 180);
+	d->fov_ratio = tan(to_rad(d->camera.fieldofview / 2));
 	d->camera.x_axis = cross((t_3d){0, 1, 0}, reverse(d->camera.vector));
 	d->camera.y_axis = cross(d->camera.x_axis, d->camera.vector);
 }
